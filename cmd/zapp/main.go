f3c48e05dbc534826f3a3a7cbac96f334f0733ac
dvl0p <245560527+dvl0p@users.noreply.github.com>
2026-05-08 13:28:04 +0200

    feat: get inode

--- cmd/zapp/main.go
+++ cmd/zapp/main.go
@@ -4,14 +4,44 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
+	"bytes"
+	"iter"
 )
 
 
 func main() {
 	output, err := os.ReadFile("/proc/net/tcp")
 	if err != nil {
-		fmt.Println("ERROR:", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-	fmt.Print(string(output))
+	filtered := getInodes(output)
+	for i := range filtered {
+		fmt.Print(string(filtered[i]),"\n")
+	}
 }
+
+
+func getInodes(content []byte) [][]byte {
+	inodes := [][]byte{}
+	next, stop := iter.Pull(bytes.Lines(content))
+	defer stop()
+	_, ok := next()
+	if !ok {
+		return inodes
+	}
+	for {
+		line, ok := next()
+		if !ok {
+			break
+		}
+		fields := bytes.Fields(line)
+		if !bytes.Equal(fields[3],[]byte("0A")) {
+			break
+		}
+		inodes = append(inodes, fields[9])
+	}
+	return inodes
+}
+
+
